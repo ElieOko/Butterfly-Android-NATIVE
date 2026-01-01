3079f1de0d8da6a03058429ca672d53347471ca8
ElieOko <elieoko100@gmail.com>
2026-01-01 18:49:09 +0100

    install:ktor,room,ksp,navigation,firebase,accompanist,lifecycle,coroutine,preference datastore,gson,contraint layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
+import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+    id("com.google.devtools.ksp")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -28,12 +33,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    
+//    kotlinOptions {
+//        jvmTarget = "11"
+//        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2.toString()
+//    }
     buildFeatures {
         compose = true
     }
@@ -49,6 +56,46 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //constraint-layout
+    implementation(libs.androidx.constraintlayout.compose)
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    //gson
+    implementation(libs.gson)
+    // datastore
+    implementation(libs.androidx.datastore.preferences)
+    //ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.logging.jvm)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    //auth
+    implementation(libs.ktor.client.auth)
+    //Coroutines
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.kotlinx.coroutines.play.services)
+    // lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+    implementation (libs.androidx.lifecycle.extensions)
+    implementation( libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.activity.ktx)
+    // room
+    implementation(libs.androidx.room.runtime)
+    //implementation(libs.firebase.crashlytics.gradle)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.paging)
+    implementation(libs.androidx.room.ktx)
+    // Accompanist Permissions
+    implementation(libs.accompanist.permissions)
+    //firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
