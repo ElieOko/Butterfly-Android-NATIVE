3dc229460add3776b164a4b731f75fa98e45e89e
ElieOko <elieoko100@gmail.com>
2026-01-02 13:02:28 +0100

    feat:proprièté élementaire pour la connexion avec un serveur distant

---
+++ app/src/main/java/elieoko/me/butterfly/core/network/Remote.kt
@@ -0,0 +1,8 @@
+package elieoko.me.butterfly.core.network
+
+object Remote {
+     val baseUrl = "https://dummyjson.com/"
+     val hostDev = "213.136.74.84:8090"
+     val hostProd = "custom.cadeauparfait1.com/api"
+     val isProd = false
+}
