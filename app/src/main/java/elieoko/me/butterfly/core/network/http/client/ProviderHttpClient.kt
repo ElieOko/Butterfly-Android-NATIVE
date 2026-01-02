2d84e542b3165c7f31e536a6787a02216d35d7c2
ElieOko <elieoko100@gmail.com>
2026-01-02 13:03:45 +0100

    feat:implementation du fournisseur de http client dans l'approche de l'adaptateur

---
+++ app/src/main/java/elieoko/me/butterfly/core/network/http/client/ProviderHttpClient.kt
@@ -0,0 +1,7 @@
+package elieoko.me.butterfly.core.network.http.client
+
+import elieoko.me.butterfly.core.network.http.client.ktor.KtorClientAndroid
+
+object ProviderHttpClient {
+    val client = KtorClientAndroid()
+}
