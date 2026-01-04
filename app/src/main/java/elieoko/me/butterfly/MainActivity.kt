c86922f1e7e330b187ce8d2e1d996a3386db8f86
ElieOko <elieoko100@gmail.com>
2026-01-04 23:57:34 +0100

    fix:viewmodel network state

--- app/src/main/java/elieoko/me/butterfly/MainActivity.kt
+++ app/src/main/java/elieoko/me/butterfly/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.*
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import elieoko.me.butterfly.core.network.connectivity.AndroidConnectivityObserver
+import elieoko.me.butterfly.core.network.connectivity.ConnectivityViewModel
 import elieoko.me.butterfly.core.ui.theme.ButterflyTheme
 import elieoko.me.butterfly.view.navigation.NavigationRoot
 
@@ -14,6 +19,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val connectivityViewModel = viewModel<ConnectivityViewModel> {
+                ConnectivityViewModel(
+                    connectivityObserver = AndroidConnectivityObserver(
+                        context = applicationContext
+                    )
+                )
+            }
+            val isConnected by connectivityViewModel.isConnected.collectAsStateWithLifecycle()
             ButterflyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavigationRoot(modifier = Modifier
