43d01db8e7fe8f525f7ead773d963837b61bdd95
ElieOko <elieoko100@gmail.com>
2026-01-04 23:51:41 +0100

    feat:meditation detail composant

---
+++ app/src/main/java/elieoko/me/butterfly/app/meditation/application/ui/MeditationDetail.kt
@@ -0,0 +1,21 @@
+package elieoko.me.butterfly.app.meditation.application.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.NavKey
+
+@Composable
+fun MeditationDetail(key: NavKey) {
+    MeditationDetailBody()
+}
+
+@Composable
+fun MeditationDetailBody(){
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MeditationDetailPreview(){
+    MeditationDetail(it)
+}
