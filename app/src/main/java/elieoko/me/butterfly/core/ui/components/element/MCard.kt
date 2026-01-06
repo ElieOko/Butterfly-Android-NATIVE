5e1f9749145be51a597a340cf65ab362372d1d3e
ElieOko <elieoko100@gmail.com>
2026-01-06 23:59:24 +0100

    feat:nouvelle structure du card

--- app/src/main/java/elieoko/me/butterfly/core/ui/components/element/MCard.kt
+++ app/src/main/java/elieoko/me/butterfly/core/ui/components/element/MCard.kt
@@ -1,7 +1,10 @@
 package elieoko.me.butterfly.core.ui.components.element
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Start
@@ -13,8 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import elieoko.me.butterfly.R
 
 @Composable
@@ -22,10 +28,12 @@ import elieoko.me.butterfly.R
 fun MCard(){
     Card {
         Column(
+            modifier = Modifier.padding(4.dp),
             horizontalAlignment = Alignment.CenterHorizontally) {
             Icon(Icons.Outlined.Start,null)
             Text("Verse of the day")
-            Text("I am the true vine, and my Father is the husbandman.")
+            Spacer(Modifier.height(10.dp))
+            Text("I am the true vine, and my Father is the husbandman.", fontSize = 19.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
             SuggestionChip(
                 label = {Text("Jean 15")},
                 onClick = {},
